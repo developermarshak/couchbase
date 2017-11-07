cb706780c16d539451147d9686173ad06639a666
Nikita Marshak <n.marshak@conci.info>
2017-11-07 16:03:40 +0200

    fix couchbase uri

--- tests/create-bucket.php
+++ tests/create-bucket.php
@@ -3,8 +3,6 @@
 $config = require __DIR__."/config/database.php";
 
 $couchbase = $config['connections']['couchbase'];
-$connection = new Couchbase\Cluster($couchbase['host']);
+$connection = new Couchbase\Cluster("couchbase://".$couchbase['host'].":".$couchbase['port']);
 
 $connection->manager($couchbase["user"], $couchbase["password"])->createBucket($couchbase["bucketname"]);
-
-echo "Couchbase bucket created!\n";
