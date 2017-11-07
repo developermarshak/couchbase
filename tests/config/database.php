360ae8e353c6140c24bbf9c9b965a73a91527a82
Nikita Marshak <n.marshak@conci.info>
2017-11-07 10:12:09 +0200

    add access to couchbase. Alias for couchbase

--- tests/config/database.php
+++ tests/config/database.php
@@ -8,21 +8,12 @@ return [
             'name'       => 'couchbase',
             'driver'     => 'couchbase',
             'port'       => '8091',
-            'host'       => '127.0.0.1',
-            'bucket'     => 'testing',
-            'n1ql_hosts' => ['http://127.0.0.1:8093']
-        ],
-
-        'mysql' => [
-            'driver'    => 'mysql',
-            'host'      => '127.0.0.1',
-            'database'  => 'testing',
-            'username'  => 'root',
-            'password'  => '',
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ],
+            'host'       => 'couchbase',
+            'bucket'     => 'testbucket',
+            'user'       => 'conci',
+            'password'   => 'devtest',
+            'n1ql_hosts' => ['http://couchbase:8093']
+        ]
     ],
 
 ];
