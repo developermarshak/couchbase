2665c569a2938679a7381cc3e045f53dd8a91dce
Nikita Marshak <n.marshak@conci.info>
2017-11-08 16:28:28 +0200

    add primary key.

--- tests/create-bucket.php
+++ tests/create-bucket.php
@@ -1,10 +1,41 @@
 <?php
 
-$config = require __DIR__."/config/database.php";
+class CreateBucketHelper{
+    protected $bucketName;
+    protected $config;
 
-$couchbase = $config['connections']['couchbase'];
-$cluster = new Couchbase\Cluster("couchbase://".$couchbase["host"].":".$couchbase["port"]);
+    /**
+     * @var Couchbase\Cluster
+     */
+    protected $cluster;
+    function __construct()
+    {
+        $globalConfig = require __DIR__."/config/database.php";
 
-$manager = $cluster->manager("conci", "devpass");
+        $this->config = $globalConfig['connections']['couchbase'];
+        $this->bucketName = $this->config['bucket'];
+    }
 
-$manager->createBucket("test-bucket");
+    function init(){
+        $this->cluster = $this->connection();
+        $this->createBucket();
+        $this->createPrimaryIndex();
+    }
+
+    protected function connection(){
+        return new Couchbase\Cluster("couchbase://".$this->config["host"].":".$this->config["port"]);
+    }
+
+    protected function createBucket(){
+        $manager = $this->cluster->manager($this->config["user"], $this->config["password"]);
+        $manager->createBucket($this->bucketName);
+    }
+
+    protected function createPrimaryIndex(){
+        $bucket = $this->cluster->openBucket($this->bucketName);
+        $bucket->manager()->createN1qlPrimaryIndex($this->bucketName."-primary-index");
+    }
+}
+
+$helper = new CreateBucketHelper();
+$helper->init();
