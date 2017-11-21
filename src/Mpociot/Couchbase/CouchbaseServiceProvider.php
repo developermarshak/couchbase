305ec5e262567547ffe9180fd289124f6f8e1948
Nikita Marshak <n.marshak@conci.info>
2017-11-21 14:40:42 +0200

    Add migrate

--- src/Mpociot/Couchbase/CouchbaseServiceProvider.php
+++ src/Mpociot/Couchbase/CouchbaseServiceProvider.php
@@ -4,6 +4,7 @@ use Illuminate\Support\ServiceProvider;
 use Mpociot\Couchbase\Eloquent\Model;
 use Mpociot\Couchbase\Connection as CouchbaseConnection;
 use Illuminate\Database\Connection as IlluminateConnection;
+use Mpociot\Couchbase\Listeners\MigrateListener;
 
 class CouchbaseServiceProvider extends ServiceProvider
 {
@@ -24,18 +25,21 @@ class CouchbaseServiceProvider extends ServiceProvider
     {
         // Add database driver.
         $this->app->singleton('couchbase.connection', function($app){
-            $connectionName = config('database.connections.couchbase');
-            return new CouchbaseConnection($connectionName);
+            $config = config('database.connections.couchbase');
+            return new CouchbaseConnection($config);
         });
 
-        IlluminateConnection::resolverFor('couchbase', function ($config) {
-            return app('couchbase.connection');
-        });
+//        IlluminateConnection::resolverFor('couchbase', function ($config) {
+//            return app('couchbase.connection');
+//        });
 
         $this->app->resolving('db', function ($db) {
             $db->extend('couchbase', function ($config) {
                 return app('couchbase.connection');
             });
         });
+
+        //Add migrate listeners
+        $this->app['events']->listen(["\Illuminate\Console\Events\CommandStarting"], MigrateListener::class);
     }
 }
